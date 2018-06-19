5981e550c2b0711eee716e363c5e71589b3a03e4
mthwbrwn <mthwbrwn@gmail.com>
2018-06-19 08:35:13 -0700

    css layout being added as wireform

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-{/* <li> <img id = "onePhoto" src="http://via.placeholder.com/300x300" alt=""> </li>
+'use strict'
+
+/* <li> <img id = "onePhoto" src="http://via.placeholder.com/300x300" alt=""> </li>
 <li> <img id = "twoPhoto" src="http://via.placeholder.com/300x300" alt=""> </li>  
-<li> <img id = "threePhoto" src="http://via.placeholder.com/300x300" alt=""> </li> */}
+<li> <img id = "threePhoto" src="http://via.placeholder.com/300x300" alt=""> </li> */
 
 
 // STEPS
@@ -16,17 +18,30 @@ function photoSelect(label, path, access) {
   photoSelect.photoArray.push(this);
 }
 
+// get elements
+photoSelect.imgOneElement = document.getElementById('onePhoto');
+photoSelect.imgTwoElement = document.getElementById('twoPhoto');
+photoSelect.imgThreeElement = document.getElementById('threePhoto');
+
+
 //need event listener for 'click'
 
 // track number of clicks
 //track number of displays 
 
+photoSelect.randomPhoto = function(elPic) {
+  var randomArrayNum = Math.trunc(Math.random() * photoSelect.photoArray.length);
+  
+  var pickPhoto = photoSelect.photoArray[randomArrayNum];
+  photoSelect.elPic.src = pickPhoto.path;
+};
+
 // set array for photos ( use constructor)
-photoSelect.bag = new photoSelect ('bag', '../img/bag' , 'a bag shaped like R2-D2' );
+photoSelect.bag = new photoSelect('bag', '../img/bag' , 'a bag shaped like R2-D2' );
 
-photoSelect.banana = new photoSelect ('banana', '../img/banana' , 'a banana shaped banana slicer' );
+photoSelect.banana = new photoSelect('banana', '../img/banana' , 'a banana shaped banana slicer' );
 
-
+// Goat.imgElement.addEventListener
 
 
 
@@ -37,9 +52,8 @@ photoSelect.banana = new photoSelect ('banana', '../img/banana' , 'a banana shap
 
 // <!-- set up js to get element by ID -->
 
-// <!-- set up random number  -->
-
+console.log(pickPhoto);
 // test instantiation
-
+photoSelect.randomPhoto(imageOneElement);
 
 console.log(photoSelect.photoArray);
