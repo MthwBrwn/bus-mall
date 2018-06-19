32ef7a5beac97a1fb9976266db7ed291e09c5e69
mthwbrwn <mthwbrwn@gmail.com>
2018-06-19 16:07:14 -0700

    got image loading correctly

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bus-mall
 This is a market analysis app that presents 3 pictures and asks user to select their favorite
+
+
+Images used : 
+https://pixabay.com/en/photos/bus/
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /* <li> <img id = "onePhoto" src="http://via.placeholder.com/300x300" alt=""> </li>
 <li> <img id = "twoPhoto" src="http://via.placeholder.com/300x300" alt=""> </li>  
@@ -8,52 +8,69 @@
 // STEPS
 
 // set up variables global? spec.to const. Object?
-photoSelect.photoArray = [];
+
+PhotoSelect.photoArray = [];
+// get Element for list
+PhotoSelect.listEl = document.getElementById('listBuild');
+
 
 // set up constructor method for pictures
-function photoSelect(label, path, access) {
+function PhotoSelect(name, path, altText) {
   this.name= name;
   this.path= path;
-  this.access= access;
-  photoSelect.photoArray.push(this);
+  this.altText= altText;
+  PhotoSelect.photoArray.push(this);
+
+  console.log(PhotoSelect.photoArray);
 }
 
 // get elements
-photoSelect.imgOneElement = document.getElementById('onePhoto');
-photoSelect.imgTwoElement = document.getElementById('twoPhoto');
-photoSelect.imgThreeElement = document.getElementById('threePhoto');
+PhotoSelect.imgOneElement = document.getElementById('onePhoto');
+PhotoSelect.imgTwoElement = document.getElementById('twoPhoto');
+PhotoSelect.imgThreeElement = document.getElementById('threePhoto');
 
 
-//need event listener for 'click'
 
-// track number of clicks
-//track number of displays 
-
-photoSelect.randomPhoto = function(elPic) {
-  var randomArrayNum = Math.trunc(Math.random() * photoSelect.photoArray.length);
+// track number of clicks by user (25)
+//track number of displays cannot have repeats
+PhotoSelect.randomPhoto = function() {
+  var randomArrayNum = Math.trunc(Math.random() * PhotoSelect.photoArray.length);
+  console.log (randomArrayNum);
   
-  var pickPhoto = photoSelect.photoArray[randomArrayNum];
-  photoSelect.elPic.src = pickPhoto.path;
+  // set the src for the random picture 
+  PhotoSelect.imgOneElement.src = PhotoSelect.photoArray[randomArrayNum].path;
 };
 
+// removeEvent listener
+
 // set array for photos ( use constructor)
-photoSelect.bag = new photoSelect('bag', '../img/bag' , 'a bag shaped like R2-D2' );
+new PhotoSelect('bag', 'img/bag.jpg' , 'a bag shaped like R2-D2' );
 
-photoSelect.banana = new photoSelect('banana', '../img/banana' , 'a banana shaped banana slicer' );
+// new PhotoSelect('banana', '../img/banana' , 'a banana shaped banana slicer' );
 
-// Goat.imgElement.addEventListener
+// new PhotoSelect('bathroom', '../img/bathroom' , 'a combination toilet paper roll/ tablet holder' );
+
+// new PhotoSelect('boots', '../img/boots' , 'soleless and toeless rain boots ' );
+
+// new PhotoSelect('breakfast', '../img/breakfast' , 'combination oven egg pan and coffee pot' );
+
+// new PhotoSelect('bubblegum', '../img/bubblegum' , 'meatball flavored bubble gum' );
+
+
+// Goat.imgElement.addEventListener 
+//need event listener for 'click'
 
 
 
 // need way to prevent similar selections (for loop)
 // once 25 steps are done, - dispay results to user 
 
-
+// increment number of times displayed
 
 // <!-- set up js to get element by ID -->
 
-console.log(pickPhoto);
-// test instantiation
-photoSelect.randomPhoto(imageOneElement);
 
-console.log(photoSelect.photoArray);
+// test instantiation
+PhotoSelect.randomPhoto();
+
+
