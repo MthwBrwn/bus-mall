3e86f2f9e030fa091ffabb99fec2b4cc66a53d2d
mthwbrwn <mthwbrwn@gmail.com>
2018-06-21 04:20:35 -0700

    chart works - does not present all data

--- js/app.js
+++ js/app.js
@@ -155,7 +155,7 @@ PhotoSelect.randomPhoto();
 PhotoSelect.photoEventElement.addEventListener('click', PhotoSelect.clickAction);
 
 PhotoSelect.renderChart = function () {
-  var context = document.getElementById('results-chart'). getContext('2d');
+  // var context = document.getElementById('results-chart'). getContext('2d');
   var chartColors =  [
     '#e6194b',
     '#3cb44b',
@@ -177,86 +177,64 @@ PhotoSelect.renderChart = function () {
     '#ffd8b1',
     '#000080',
     '#808080',
-    '#000000',
-    
+    '#000000'
   ];
 
-  var photoChart = new Chart(context, {
-    type: 'bar',
-    dataPointMaxWidth: 5,
-    data: {
-      labels: PhotoSelect.names,
-      datasets: [{
-        label: 'votes for Photos',
-        data: PhotoSelect.totalVotes,
-        color: chartColors,
-      }],
-    },
-    options:{
-      scales:{
-        yAxes:[{
+  // var photoChart = new Chart(context, {
+  //   type: 'bar',
+  //   // dataPointMaxWidth: 5,
+  //   data: {
+  //     labels: PhotoSelect.names,
+  //     datasets: [{
+  //       label: 'votes for Photos',
+  //       data: PhotoSelect.totalVotes,
+  //       color: chartColors
+  //     }],
+  //   },
+  //   options:{
+  //     scales:{
+  //       yAxes:[{
           
-          tick:{
-            beginAtZero: true,
-            tickLength: 1,
-          }
-        }],
-        xAxes:[{
-          labelAutoFit: true,
-          labelAngle: 80,
-          tick:{
+  //         tick:{
+  //           beginAtZero: true,
+  //           tickLength: 1,
+  //         }
+  //       }],
+  //       axisX:[{
+  //         labelAutoFit: true,
+  //         labelAngle: 80
+  //         // tick:{
+  //         // }
+  //       }]
+
+
+  //     }
+  //   }
+  // });
+
+// };
+
+  var ctx = document.getElementById("results-chart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: 'Votes for Photos',
+        data: PhotoSelect.totalVotes,
+        backgroundColor: chartColors,
+        borderColor: chartColors,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
           }
         }]
-
-
       }
     }
   });
-
 };
-
-// var ctx = document.getElementById("results-chart").getContext('2d');
-// var myChart = new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255,99,132,1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [{
-//         ticks: {
-//           beginAtZero:true
-//         }
-//       }]
-//     }
-// }
-// });
-
-
-
-
-
-
-
-
